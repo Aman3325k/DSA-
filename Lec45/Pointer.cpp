405fb81fb0fd3efccd45536b019c24ff09c1788a
Aman3325k <prdhanaman2004@gmail.com>
2024-08-31 10:33:00 +0530

    Pointer basic

---
+++ Lec45/Pointer.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  // int a = 10;
+   // cout << &a << endl;
+
+  //  int *ptr = &a;
+//    cout << sizeof (ptr) << endl; 
+
+
+    float m = 4.34;
+    float *ptr1 = &m;
+    cout << ptr1 << endl;
+
+int a = 10;
+int *ptr = &a;
+cout << ptr << endl; //address of a
+cout <<*ptr << endl; //value inside 
+
+}
