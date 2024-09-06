293529241e4e708796894ba044475703c8a74dbf
Aman3325k <prdhanaman2004@gmail.com>
2024-09-06 19:09:27 +0530

    MemoryManagement

---
+++ Lec49/MemoryManagement.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int *ptr = new int;
+    *ptr = 5;
+    cout << *ptr << endl;
+
+    float *ptr2 = new float;
+    *ptr2 = 3.5;
+    cout << *ptr2 << endl;
+
+    int n;
+    cout << "Enter the size of array :";
+    cin >> n;
+ 
+    
+    int *p = new int [n];
+  
+    //value daalo
+    for (int i = 0; i < n;i++)
+    p[i]=i+1;
+
+      //print kara do
+      for(int i=0;i<n;i++)
+      cout << p[i] << endl;
+
+    //delete keyword use
+
+    delete ptr;
+    delete ptr2;
+    delete[] p;
+}
